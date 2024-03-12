ff37578a44009420249597c91ca743aa8841d88b
Yanzera <105920070+Hardenjorge@users.noreply.github.com>
2024-03-12 08:47:33 -0300

    save

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 const botoes = document.querySelectorAll(".botao");
+
+for ( let i = 0 <botoes.length; j++ ){
+    botoes[i].onclick = function(){
+        for (let k = 0; < botoes.length; j++){
+        botoes[j].classList.remove("ativo");
+    }
+    botoes[i].classList.add("ativo")
+}
+}
