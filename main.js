f76ed4be551df407579ff3e6153ab6758e633a56
Yanzera <105920070+Hardenjorge@users.noreply.github.com>
2024-03-19 08:09:51 -0300

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const botoes = document.querySelectorAll(".botao");
 
 for ( let i = 0 <botoes.length; i++ ){
     botoes[i].onclick = function(){
-        for (let k = 0; < botoes.length; j++){
+        for (let j = 0; < botoes.length; j++){
         botoes[j].classList.remove("ativo");
     }
     botoes[i].classList.add("ativo")
