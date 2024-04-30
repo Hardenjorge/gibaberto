d06fe69d72e041c3b0e666e300fe92f9c37f83a5
Yanzera <105920070+Hardenjorge@users.noreply.github.com>
2024-04-30 07:29:30 -0300

    save

--- main.js
+++ main.js
@@ -11,3 +11,9 @@ for(let i = 0; i <botoes.length; i++){
 
     }
 }
+
+const contadores = document.querySelectorAll (".contador");
+const tempoObjetivo1 = new Date ("2024-12-22");
+let tempoAtual = new Date();
+
+contadores[0].textContent = tempoObjetivo1 - tempoAtual;
